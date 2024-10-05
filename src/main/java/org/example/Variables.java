df642a34f0985addc52f94e38d4ee00e343c6310
Sandhya <penkesr24@gmail.com>
2024-10-04 20:15:55 -0500

    Oct3rd-Commit

---
+++ src/main/java/org/example/Variables.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.google.common.base.Strings;
+
+public class Variables {
+    public static void main(String[] args){
+        int a = 100;
+        String name = "John";
+        float c = 2.9f;
+        char d = 'S';
+        boolean myBool = true;
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(myBool);
+
+    }
+}
