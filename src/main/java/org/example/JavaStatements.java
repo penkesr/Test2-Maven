acbf5b372bbc747405c1c2e89b3b2a39b0e5fd51
Sandhya <penkesr24@gmail.com>
2024-10-04 21:30:09 -0500

    Oct3rd-Commit

--- src/main/java/org/example/JavaStatements.java
+++ src/main/java/org/example/JavaStatements.java
@@ -37,10 +37,52 @@ public class JavaStatements {
                 System.out.println("Sunday");
                 break;
         }
+        System.out.println("*** Practicing While-Do Statement- Print 0 to 4 ***");
         int i = 0;
         while (i < 5) {
             System.out.println(i);
             i++;
+        }
+        System.out.println("*** Practicing Do-While Statement- Print 0 to 5 ***");
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        }
+        while (j < 6);
+
+        System.out.println("*** Practicing Do-While Statement- Print 10 to 14 ***");
+        for (int k = 10; k < 15; k++) {
+            System.out.println(k);
+
+            System.out.println("*** Practicing Nested Loops ***");
+            // Outer loop.
+            for (int a = 100; a <= 102; a++) {
+                System.out.println("Outer: " + a); // Executes 2 times
+
+                // Inner loop
+                for (int b = 103; b <= 105; b++) {
+                    System.out.println(" Inner: " + b); // Executes 6 times (2 * 3)
+
+                    System.out.println("*** Practicing For-Each Loop ***");
+                    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+                    for (String Carname : cars) {
+                        System.out.println(Carname);
+
+                        System.out.println("*** Practicing Break-Continue statements ***");
+                        for (int p = 40; p < 45; p++) {
+                            if (p == 45) {
+                                break;
+                            }
+                            System.out.println(p);
+
+                            System.out.println("*** Practicing Arrays ***");
+                            String[] arraycars = {"Volvo", "BMW", "Ford", "Mazda"};
+                            System.out.println(arraycars.length);
+                        }
+                    }
+                }
+            }
         }
     }
 }
