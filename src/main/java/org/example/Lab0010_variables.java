9ecc3324c1a216ed49f1ec726096affc4d02d2de
Sandhya <penkesr24@gmail.com>
2024-10-07 20:27:15 -0500

    Oct3rd-Commit

---
+++ src/main/java/org/example/Lab0010_variables.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Lab0010_variables {
+    public static void main(String[] args) {
+
+        //variable with lowercase
+        int age = 10;
+        //variagle name followed by number
+        int age12 = 30;
+        //variagle name followed by $
+        int age$ = 40;
+        //variagle name followed by _
+        int age_ = 50;
+        //variagle name as _1
+        int _1 =60;
+        //variable with capital
+        int NAME = 80;
+        //variable with 2 names with $ in between
+        int FN$LN = 90;
+
+        System.out.println(" The value of age "+age);
+        System.out.println(" The value of age12 "+age12);
+        System.out.println(" The value of age$ "+age$);
+        System.out.println(" The value of age_ "+age_);
+        System.out.println(" The value of _1 "+_1);
+        System.out.println(" The value of NAME "+NAME);
+        System.out.println(" The value of FN$LN "+FN$LN);
+
+
+    }
+}
---
+++ src/main/java/org/example/Lab0011_datatypes.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.apache.xpath.operations.Bool;
+
+public class Lab0011_datatypes {
+    public static void main(String[] args) {
+        //Primitive - System defined
+        boolean FirstName = true;
+        byte LN =  100; // limit is -128 to 128 only [decimal system]
+        byte age = 122;
+        short s =1000;
+        int i = 109999; //int can store
+        long phonenumber = 6146871081;
+        float percentage = 75.2f;
+        char Name = 'S';
+        System.out.println(");
+
+        // Non Primitive - User defined
+        String Fullname = "Sandhya"
+
+
+
+
+
+
+
+
+
+
+    }
+}
